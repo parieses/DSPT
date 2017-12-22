627639e56c71763b41bfc6945a5d6142a223cc74
ll <1695699447@qq.com.com>
2017-12-22 11:35:09 +0800

    userLogin

--- app/Http/Controllers/Home/LoginController.php
+++ app/Http/Controllers/Home/LoginController.php
@@ -57,9 +57,9 @@ class LoginController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function user_login($id)
     {
-        //
+        dd(1);
     }
 
     /**
